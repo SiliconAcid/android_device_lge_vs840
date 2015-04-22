fb7cc1249e876ff68e121c3813aee3404b634dc5
liv3dn8as <twcoplien@gmail.com>
2015-04-22 14:31:36 -0500

    moving to bs

--- bs.mk
+++ bs.mk
@@ -1,10 +1,3 @@
-# Custom apns must come before cdma.mk is included
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/config/apns-conf.xml:system/etc/apns-conf.xml
-
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/beanstalk/config/cdma.mk)
-
 # Boot animation
 TARGET_SCREEN_HEIGHT := 480
 TARGET_SCREEN_WIDTH := 800
@@ -12,19 +5,22 @@ TARGET_SCREEN_WIDTH := 800
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/beanstalk/config/common_full_phone.mk)
 
+# Enhanced NFC
+$(call inherit-product, vendor/beanstalk/config/nfc_enhanced.mk)
+
 # Inherit device configuration
 $(call inherit-product, device/lge/vs840/vs840.mk)
 
-PRODUCT_NAME := bs_vs840
-
-# Release name and versioning
-PRODUCT_RELEASE_NAME := Lucid
-PRODUCT_VERSION_DEVICE_SPECIFIC :=
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vs840
+PRODUCT_NAME := bs_vs840
+PRODUCT_BRAND := LGE
+PRODUCT_MODEL := LG-VS840
+PRODUCT_MANUFACTURER := lge
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=i_vzw BUILD_FINGERPRINT=Verizon/i_vzw/i_vzw:4.0.4/IMM76D/VS840ZV7.47e5065f:user PRIVATE_BUILD_DESC="i_vzw-user 4.0.4 IMM76D 47e5065f release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   BUILD_FINGERPRINT=i_vzw/i_vzw:4.0.4/IMM76D/VS840ZV7.47e5065f:user/release-keys \
+   PRIVATE_BUILD_DESC="i_vzw-user 4.0.4 IMM76D 47e5065f release-keys"
 
 # Enable Torch
 PRODUCT_PACKAGES += Torch WiFiDirectDemo
