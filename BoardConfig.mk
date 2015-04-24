ad5ee269dd3aa4e7ba5d8b41d5a899d548f5dc59
liv3dn8as <twcoplien@gmail.com>
2015-04-24 12:43:41 -0500

    fix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,10 +1,10 @@
-include device/lge/iprj-common/BoardConfigCommon.mk
+# inherit from the proprietary version
+include vendor/lge/vs840/BoardConfigVendor.mk
 
 TARGET_KERNEL_CONFIG := i_vzw_defconfig
 
-TARGET_OTA_ASSERT_DEVICE := vs840
+TARGET_OTA_ASSERT_DEVICE := vs840,lucid
 
-I_DTS_TARGET := msm8660-perf_defconfig
+LUCID_DTS_TARGET := msm8660-perf_defconfig
 
-WITH_DEXPREOPT := false
 
